3fa1b15f9e6ecc2868c08bdc810a09d39c21ddc1
Muchaaaaa <sylviaslyg@gmail.com>
2021-07-26 14:26:41 +0300

    js functiond added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function onsubmit(){
-    
-}
+var validation = function () {
+    let Name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+  
+    if (Name === "" || email === "") {
+      alert("Enter your details");
+    } else {
+      alert("Message received");
+    }
+  };
