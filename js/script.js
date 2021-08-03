c5a2b238ba46793af93189fd24df450e836088c2
Muchaaaaa <sylviaslyg@gmail.com>
2021-08-03 14:20:52 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -1,46 +1,15 @@
-$(".design-img").click(function(){
-  $(".design-img").toggle(800);
-  $(".p-design").toggle(800);
-});
 
-$(".p-design").click(function(){
-  $(".design-img").toggle(800);
-  $(".p-design").toggle(800);
-});
-
-$(".development-img").click(function(){
-  $(".development-img").toggle(800);
-  $(".p-development").toggle(800);
-});
-
-$(".p-development").click(function(){
-  $(".development-img").toggle(800);
-  $(".p-development").toggle(800);
-});
-
-$(".management-img").click(function(){
-  $(".management-img").toggle(800);
-  $(".p-management").toggle(800);
-});
-
-$(".p-management").click(function(){
-  $(".management-img").toggle(800);
-  $(".p-management").toggle(800);    
-});
-
-
-
-let collectInput = () => {
+let details=function(){
   if ($("#name").val() == "") {
       alert("Enter your name please");
   } else if ($("#email").val() == "") {
       alert("Enter your email please")
   }
-   else if ($("#userMessage").val() == "") {
-      alert("Your feedback is appreciated")
+   else if ($("comment").val() == "") {
+      alert("Your feedback will be appreciated")
   }
    else {
-  userName = $("#name").val();
+ let  userName = $("#name").val();
   alert(userName + " we have recieved your message.Thank you.")
   }
 }
